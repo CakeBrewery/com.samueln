f4fcf50462daca6332a5bfb27cb0212602437971
CakeBrewery <cakebrewery@gmail.com>
2015-08-24 23:25:19 -0700

    forgot to add some files

---
+++ client/app/scripts/controllers/experience.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name samuelncomApp.controller:ExperienceCtrl
+ * @description
+ * # ExperienceCtrl
+ * Controller of the samuelncomApp
+ */
+angular.module('samuelncomApp')
+  .controller('ExperienceCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
